5302a698798fe3667944d458ba8c5cd2d53cdb36
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-02-04 00:31:01 -0600

    Added a some remaining scripts

--- get-working.sh
+++ get-working.sh
@@ -8,6 +8,8 @@
 # Create a diretory variable
 dir="$(dirname "$0")"
 
+GIT="$dir/lists/git.list"
+GSETTINGS="$dir/lists/gsettings.list"
 INSTALL="$dir/lists/install.list"
 REMOVE="$dir/lists/remove.list"
 
@@ -20,6 +22,16 @@ apt-get remove -y $(cat $REMOVE)
 apt-get install -y $(cat $INSTALL)
 apt-get -y autoclean && sudo apt-get -y autoremove
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+
+echo vm.swappiness=10 | tee -a /etc/sysctl.conf
+echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf
+sysctl -p
+
+
+ln -s /usr/bin/terminator /usr/bin/gnome-terminal
+
 
 while IFS= read line
 do
@@ -31,3 +43,6 @@ while IFS= read line
 do
     eval gsettings set $line
 done < "$GSETTINGS"
+
+
+service ssh restart
