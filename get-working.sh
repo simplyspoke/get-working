839126050557e203bb2853482ddd853073881c89
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-02-04 01:17:31 -0600

    Mined some additional information from more files.

--- get-working.sh
+++ get-working.sh
@@ -90,3 +90,10 @@ service ssh restart
 # echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 # sudo apt-get update
 # sudo apt-get install -y mongodb-org
+#
+#
+# sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+# curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install heroku
+# heroku login
