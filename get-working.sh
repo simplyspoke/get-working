2e14a9b9a2576a9c2616f71acdd7f02f4fb144e8
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-02-04 00:39:00 -0600

    Added other comands as comments to resolve at a future date.

--- get-working.sh
+++ get-working.sh
@@ -48,3 +48,17 @@ done < "$GSETTINGS"
 
 
 service ssh restart
+
+
+## UNRESOLVED
+## These commands have not been intergrated into the script yet.
+# 
+# echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+#
+# sudo add-apt-repository multiverse
+# sudo apt update && sudo apt install steam
+#
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+# echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+# sudo apt-get update
+# sudo apt-get install -y mongodb-org
